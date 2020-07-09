77ce11cf1b2fe359ef91631516643c9b6771318b
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-09 04:54:56 +0100

    creating servers and endpoints

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const fs = require('fs');
-
-const input = fs.readFileSync('./txt/input.txt', 'utf-8');
-
-const textOut = `created file on ${Date.now()} \n  ${input}`;
-fs.writeFileSync('./txt/output.txt', textOut); 
-fs.writeFileSync('./txt/output1.txt', textOut); 
+const http = require('http');
+const url = require('url');
 
 
-console.log(input);
+// server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const productObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if(pathName === '/'  || pathName === '/overview'){
+        res.end('this is the overview page')
+    }else if(pathName === '/product'){
+        res.end('this is the product page')
+    }else if(pathName === '/api'){
+        res.writeHead(200, {
+            'Content-type': 'application/json',
+        })
+        res.end(data);
+    }else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+        })
+        res.end('<h1>page not found</h1>')
+    }
+})
+
+server.listen(8000, '127.0.0.1');
+
+ 
