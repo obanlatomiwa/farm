903e469754afaab26a40beb18c627dd392a6baee
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-09 17:35:58 +0100

    project page done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// core modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+// my modules
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // templates
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -11,25 +15,10 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const productObj = JSON.parse(data);
 
-// replace template
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 
 // server
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true);
-    console.log(query);
-
     // overview page
     if(pathname === '/'  || pathname === '/overview'){
         res.writeHead(200, {
@@ -38,7 +27,6 @@ const server = http.createServer((req, res) => {
         const cardsHtml = productObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
-        
     // product page
     }
     else if(pathname === '/product'){
